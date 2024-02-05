68c245bf01874ec4746b575d12e368ff35732388
mdrmono <mdrmono29@gmail.com>
2024-02-05 09:29:55 -0500

    1.6

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,49 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      
+      <h1>statistics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+
+
+    </div>
   )
 }
 
--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,9 +1,6 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App counter={counter} />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
